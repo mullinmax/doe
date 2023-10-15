ce5e5dc5de654bf1a13e24959e04dbe44622c10f
Maxwell Mullin <mullinmax@gmail.com>
2023-10-15 14:48:27 -0400

    piodide

--- script.js
+++ script.js
@@ -1,88 +1,80 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('add-factor-btn').addEventListener('click', addFactorInput);
-    document.getElementById('generate-array-btn').addEventListener('click', generateAndDisplayArray);
+document.getElementById('add-factor-btn').addEventListener('click', addFactorInput);
+document.getElementById('generate-array-btn').addEventListener('click', generateAndDisplayArray);
 
-    let factorCount = 0;
+let factorCount = 0;
 
-    function addFactorInput() {
-        factorCount++;
-        const factorDiv = document.createElement('div');
-        factorDiv.className = 'factor';
-        factorDiv.innerHTML = `
-            <label>Factor ${factorCount} Levels (comma separated):</label>
-            <input type="text" placeholder="e.g. 1,2,4 or H,M,L">
-        `;
-        document.getElementById('factors-container').appendChild(factorDiv);
+function addFactorInput() {
+    factorCount++;
+    const factorDiv = document.createElement('div');
+    factorDiv.className = 'factor';
+    factorDiv.innerHTML = `
+        <label>Factor ${factorCount} Levels (comma separated):</label>
+        <input type="text" placeholder="e.g. 1,2,4 or H,M,L">
+    `;
+    document.getElementById('factors-container').appendChild(factorDiv);
+}
+
+async function generateAndDisplayArray() {
+    const factors = Array.from(document.querySelectorAll('.factor')).map(factor => {
+        return factor.querySelector('input[type="text"]').value.split(',').length;
+    });
+
+    // Load Pyodide and Python packages
+    await loadPyodideAndPackages();
+
+    // Run the Python code using Pyodide
+    const resultFromPython = pyodide.runPython(`gsd(${JSON.stringify(factors)})`);
+
+    // Display the result in a table
+    displayArrayInTable(resultFromPython.toJs());
+}
+
+function displayArrayInTable(array) {
+    // Clear any existing array
+    const existingArray = document.getElementById('array-display');
+    if (existingArray) {
+        existingArray.remove();
     }
 
-    function generateAndDisplayArray() {
-        const factors = Array.from(document.querySelectorAll('.factor')).map(factor => {
-            return factor.querySelector('input[type="text"]').value.split(',').length;
-        });
-
-        // Call the Python function using Brython
-        const resultFromPython = __BRYTHON__.run_script(`gsd(${JSON.stringify(factors)})`);
-
-        // Parse the result and display it in a table
-        const array = JSON.parse(resultFromPython);
-        displayArrayInTable(array);
+    const arrayDiv = document.createElement('div');
+    arrayDiv.id = 'array-display';
+    let tableHeaders = '<th>Experiment</th>';
+    for (let i = 1; i <= factorCount; i++) {
+        tableHeaders += `<th>Factor ${i}</th>`;
     }
 
-    function displayArrayInTable(array) {
-        // Clear any existing array
-        const existingArray = document.getElementById('array-display');
-        if (existingArray) {
-            existingArray.remove();
-        }
-
-        const arrayDiv = document.createElement('div');
-        arrayDiv.id = 'array-display';
-        let tableHeaders = '<th>Experiment</th>';
-        for (let i = 1; i <= factorCount; i++) {
-            tableHeaders += `<th>Factor ${i}</th>`;
-        }
-
-        let tableRows = '';
-        array.forEach((row, rowIndex) => {
-            let rowData = `<td>Experiment ${rowIndex + 1}</td>`;
-            row.forEach(value => {
-                rowData += `<td>${value}</td>`;
-            });
-            tableRows += `<tr>${rowData}</tr>`;
+    let tableRows = '';
+    array.forEach((row, rowIndex) => {
+        let rowData = `<td>Experiment ${rowIndex + 1}</td>`;
+        row.forEach(value => {
+            rowData += `<td>${value}</td>`;
         });
+        tableRows += `<tr>${rowData}</tr>`;
+    });
 
-        arrayDiv.innerHTML = `
-            <h2>Generated Orthogonal Array</h2>
-            <table>
-                <thead>
-                    <tr>
-                        ${tableHeaders}
-                    </tr>
-                </thead>
-                <tbody>
-                    ${tableRows}
-                </tbody>
-            </table>
-        `;
-        document.querySelector('.container').appendChild(arrayDiv);
-    }
+    arrayDiv.innerHTML = `
+        <h2>Generated Orthogonal Array</h2>
+        <table>
+            <thead>
+                <tr>
+                    ${tableHeaders}
+                </tr>
+            </thead>
+            <tbody>
+                ${tableRows}
+            </tbody>
+        </table>
+    `;
+    document.querySelector('.container').appendChild(arrayDiv);
+}
 
-    // Load the Python code when the page loads
-    loadPythonCode();
-
-    function loadPythonCode() {
-        fetch('https://mullinmax.github.io/doe/src/gsd.py')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('dynamic-python').textContent = data;
-            // Delay Brython initialization
-            setTimeout(() => {
-                brython();
-            }, 1000);
-        })
-        .catch(error => {
-            console.error("Error loading Python code:", error);
-        });
+async function loadPyodideAndPackages() {
+    if (window.pyodide) {
+        return;
     }
     
-});
+    await loadPyodide({ indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/" });
+    await pyodide.loadPackage(['numpy']);
+    const pythonCode = await fetch('https://mullinmax.github.io/doe/src/gsd.py').then(resp => resp.text());
+    pyodide.runPython(pythonCode);
+}
